985fb73d81a76a0d92f23612ff00bf541b77c832
Patrick Sloan <patrick.sloan@kellogg.northwestern.edu>
2025-01-14 22:01:36 +0000

    labs 1-3

--- lab-2-3.sql
+++ lab-2-3.sql
@@ -1,6 +1,12 @@
 -- What was the last team (and year) to play at U.S. Cellular Field?
 
 -- Expected result:
+
+SELECT year, name
+FROM teams
+WHERE park = "U.S. Cellular Field"
+ORDER BY year DESC
+LIMIT 1;
 --
 -- +------+-------------------+
 -- | 2017 | Chicago White Sox |
